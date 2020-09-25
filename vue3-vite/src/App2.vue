b0c0d8aef10981f868242c07b30d339c13978dfa
niutong_bxbank <niutongoh@163.com>
2020-09-25 18:23:25 +0800

    vue3 vite demo

---
+++ vue3-vite/src/App2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ count }} {{ object.foo }}</div>
+  <p>
+    <button @click="myFn">按钮</button>
+  </p>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  name: 'App2',
+  // setup函数是组合API的入口函数
+  setup() {
+    let count = ref(5)
+    const object = reactive({foo: 'bar'})
+    // 在组合API中定义的变量/方法，要想在外界使用，必须通过return{xxx,xxx} 暴露出去
+    function myFn() {
+      count.value++
+    }
+    return {
+      count,
+      object,
+      myFn
+    }
+  }
+}
+</script>
---
+++ vue3-vite/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import App2 from './App2.vue'
+import './index.css'
+
+createApp(App2).mount('#app')
